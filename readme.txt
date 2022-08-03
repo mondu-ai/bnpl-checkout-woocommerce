856f1c7216beb246d1a9f39539f73fc414d5ea55
Arthur Moreira <thurmiranda@gmail.com>
2022-08-03 16:02:34 +0200

    Version bump 0.0.4 (#15)
    * Version bump
    
    * Small changes on readme

--- readme.txt
+++ readme.txt
@@ -1,22 +1,27 @@
 === mondu-checkout-woocommerce ===
-Tags: mondu, woocommerce, payment
+Contributors: mondu-ai, arthurmmoreira, tikohov20
+Tags: mondu, woocommerce, payment, bnpl, b2b
 Requires at least: 4.6
 Tested up to: 5.9.3
-Stable tag: 0.0.3
+Stable tag: 0.0.4
 Requires PHP: 7.4.0
 
 Increase your revenue with Mondu’s solution, without the operational burden.
 
 == Changelog ==
 
-=== 0.0.1 ===
+=== 0.0.4 ===
 
-* First version of plugin
+* Include direct debit payment
+
+=== 0.0.3 ===
+
+* Bugfixes
 
 === 0.0.2 ===
 
 * Included webhooks, order adjusting and more features
 
-=== 0.0.3 ===
+=== 0.0.1 ===
 
-* Bugfixes
+* First version of plugin
--- woocommerce-mondu.php
+++ woocommerce-mondu.php
@@ -3,19 +3,18 @@
  * Plugin Name: Mondu
  * Plugin URI: https://mondu.ai/
  * Description: Increase your revenue with Mondu’s solution, without the operational burden.
- * Version: 0.0.3
+ * Version: 0.0.4
  * Author: mondu
  * Author URI: https://mondu.ai
  * License: MIT
  * Text Domain: Mondu
- * Domain Path: lang
  * WC requires at least: 4.6
  * WC tested up to: 5.9.3
  */
 
 defined('ABSPATH') or die('Direct access not allowed');
 
-define('MONDU_PLUGIN_VERSION', '0.0.3');
+define('MONDU_PLUGIN_VERSION', '0.0.4');
 define('MONDU_PLUGIN_PATH', __DIR__);
 define('MONDU_VIEW_PATH', MONDU_PLUGIN_PATH . '/views');
 define('MONDU_RESSOURCES_PATH', MONDU_PLUGIN_PATH . '/resources');
