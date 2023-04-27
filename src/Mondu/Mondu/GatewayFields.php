06b33539b8886ec8de7c09c6e0080e6052384256
Arthur Moreira <thurmiranda@gmail.com>
2023-04-27 17:25:14 +0200

    Remove instruction field to make it automatically localized

--- src/Mondu/Mondu/GatewayFields.php
+++ src/Mondu/Mondu/GatewayFields.php
@@ -15,13 +15,6 @@ class GatewayFields {
         'label' => __('Enable this payment method', 'mondu'),
         'default' => 'no',
       ),
-      'instructions' => array(
-        'title' => __('Instructions', 'woocommerce'),
-        'type' => 'textarea',
-        'description' => __('Instructions that will be added to the thank you page and emails.', 'mondu'),
-        'default' => '',
-        'desc_tip' => true,
-      ),
     );
 
     return $fields;
